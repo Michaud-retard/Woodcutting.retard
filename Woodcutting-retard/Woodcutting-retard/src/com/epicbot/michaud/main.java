f5bdef09b4308b910170425052122d6363edc360
Michaud-retard <85753569+Michaud-retard@users.noreply.github.com>
2021-06-12 23:54:49 -0400

    Add files via upload

---
+++ Woodcutting-retard/Woodcutting-retard/src/com/epicbot/michaud/main.java
@@ -0,0 +1,33 @@
+package com.epicbot.michaud;
+
+import com.epicbot.api.shared.GameType;
+import com.epicbot.api.shared.model.Area;
+import com.epicbot.api.shared.model.Tile;
+import com.epicbot.api.shared.script.LoopScript;
+import com.epicbot.api.shared.script.ScriptManifest;
+
+@ScriptManifest(name = "GUI", gameType = GameType.OS)
+public class main extends LoopScript {
+
+    gui settings = new gui();
+    private final Area AREA_NORMAL_TREE = new Area(new Tile(3185, 3498, 0), new Tile(3180, 3495, 0));
+    private final Area TEST_2 = new Area(new Tile(3178, 3496, 0), new Tile(3175, 3499, 0));
+
+    @Override
+    public boolean onStart(String... strings) {
+        return true;
+    }
+
+    @Override
+    protected int loop() {
+        if (settings == null){
+            return -1;
+        } else if (settings.value.equals("Value 1")) {
+            getAPIContext().walking().walkTo(AREA_NORMAL_TREE.getCentralTile());
+        } else if (settings.value.equals("Value 2")) {
+            getAPIContext().walking().walkTo(TEST_2.getCentralTile());
+        }
+
+        return 0;
+    }
+}
